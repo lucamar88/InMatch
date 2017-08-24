f6883552efe031e7397e6c36ea80cbcc15b736e2
Luca Martelloni <lucamar1988@gmail.com>
2017-08-24 15:52:52 +0200

    tutto ok

---
+++ app/src/main/java/com/ready/sport/inmatch/Activity/MainActivity.java
@@ -0,0 +1,52 @@
+package com.ready.sport.inmatch.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.ready.sport.inmatch.R;
+import com.ready.sport.inmatch.Tools.BottomNavigationViewHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_match:
+                    mTextMessage.setText(R.string.title_match);
+                    return true;
+                case R.id.navigation_players:
+                    mTextMessage.setText(R.string.title_players);
+                    return true;
+                case R.id.navigation_player:
+                    mTextMessage.setText(R.string.title_player);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+    }
+
+}
