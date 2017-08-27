8253b87396100fdde124e55c0088983a28f823f8
Luca Martelloni <lucamar1988@gmail.com>
2017-08-27 19:37:31 +0200

    tutto ok

--- app/src/main/java/com/ready/sport/inmatch/Fragments/BasketFragment.java
+++ app/src/main/java/com/ready/sport/inmatch/Fragments/BasketFragment.java
@@ -1,10 +1,78 @@
 package com.ready.sport.inmatch.Fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.ready.sport.inmatch.R;
+import com.ready.sport.inmatch.util.CircularProgressBar;
+import com.xw.repo.BubbleSeekBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Luca Martelloni on 26/08/2017.
  */
 
 public class BasketFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.basket_fragment, container, false);
+       /* CircularProgressBar c3 = (CircularProgressBar) rootView.findViewById(R.id.circularprogressbar1);
+        c3.setTitle("7,5");
+        c3.setProgress(75);
+
+        // Spinner element
+        AppCompatSpinner spinner = (AppCompatSpinner) rootView.findViewById(R.id.spinnerSoccer);
+
+        // Spinner click listener
+        //spinner.setOnItemSelectedListener();
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Portiere");
+        categories.add("Difensore");
+        categories.add("Centrocampista");
+        categories.add("Attaccante");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        spinner.setClickable(false);
+*/
+        /*final BubbleSeekBar seekbar = (BubbleSeekBar)rootView.findViewById(R.id.fragment_treatment_Settings_programs_seekBar);
+        seekbar.getConfigBuilder()
+                .min(1)
+                .max(10.0f)
+                .floatType()
+                .progress(5.0f)
+                .sectionCount(90)
+                .trackColor(ContextCompat.getColor(getContext(), R.color.colorButton))
+                .secondTrackColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .thumbColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .showThumbText()
+                .thumbTextColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .thumbTextSize(18)
+                .bubbleColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .bubbleTextSize(18)
+                .seekBySection()
+                .showSectionMark()
+                .autoAdjustSectionMark()
+                .build();
+*/
+        return rootView;
+    }
+
 }
--- app/src/main/java/com/ready/sport/inmatch/Fragments/SoccerFragment.java
+++ app/src/main/java/com/ready/sport/inmatch/Fragments/SoccerFragment.java
@@ -3,28 +3,93 @@ package com.ready.sport.inmatch.Fragments;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.AppCompatSpinner;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ready.sport.inmatch.R;
 import com.ready.sport.inmatch.util.CircularProgressBar;
+import com.xw.repo.BubbleSeekBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Luca Martelloni on 26/08/2017.
  */
 
 public class SoccerFragment extends Fragment {
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.soccer_fragment, container, false);
         CircularProgressBar c3 = (CircularProgressBar) rootView.findViewById(R.id.circularprogressbar1);
         c3.setTitle("7,5");
         c3.setProgress(75);
+
+        // Spinner element
+        AppCompatSpinner spinner = (AppCompatSpinner) rootView.findViewById(R.id.spinnerSoccer);
+
+        // Spinner click listener
+        //spinner.setOnItemSelectedListener();
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Portiere");
+        categories.add("Difensore");
+        categories.add("Centrocampista");
+        categories.add("Attaccante");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        spinner.setClickable(false);
+
+        final BubbleSeekBar seekbar = (BubbleSeekBar)rootView.findViewById(R.id.seekbarSocVel);
+        setBubbleSeekBar(seekbar);
+        final BubbleSeekBar seekbar2 = (BubbleSeekBar)rootView.findViewById(R.id.seekbarSocPot);
+        setBubbleSeekBar(seekbar2);
+
         return rootView;
     }
+
+    public void setBubbleSeekBar(BubbleSeekBar seek){
+        seek.getConfigBuilder()
+                .min(1)
+                .max(10.0f)
+                .floatType()
+                .progress(5.0f)
+                .sectionCount(90)
+                .trackColor(ContextCompat.getColor(getContext(), R.color.colorButton))
+                .secondTrackColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .thumbColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .showThumbText()
+                .thumbTextColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .thumbTextSize(18)
+                .bubbleColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                .bubbleTextSize(18)
+                .seekBySection()
+                .showSectionMark()
+                .autoAdjustSectionMark()
+                .build();
+    }
+
 }
