cc9c6c00cfa6bdf2b267dd91f7cee6bb6b64dba1
lucamar88 <lucamar1988@gmail.com>
2017-08-31 18:09:04 +0200

    tutto ok

--- app/src/main/java/com/ready/sport/inmatch/Fragments/ListMatchFragment.java
+++ app/src/main/java/com/ready/sport/inmatch/Fragments/ListMatchFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ready.sport.inmatch.R;
+import com.ready.sport.inmatch.util.CountDownView;
 
 /**
  * Created by Luca Martelloni on 26/08/2017.
@@ -16,7 +17,9 @@ public class ListMatchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_list_match_layout, container, false);
-
+        CountDownView cdv = (CountDownView) rootView.findViewById(R.id.countdownview);
+        cdv.setInitialTime(30000); // Initial time of 30 seconds.
+        cdv.start();
         return rootView;
     }
 }
---
+++ app/src/main/java/com/ready/sport/inmatch/util/TimerListener.java
@@ -0,0 +1,10 @@
+package com.ready.sport.inmatch.util;
+
+/**
+ * Created by l.martelloni on 31/08/2017.
+ */
+
+
+public interface TimerListener {
+    void timerElapsed();
+}
